3c0560a613a757b101b29ec40630395f07893562
Bobkoskie, Robert (rb868x) <rb868x@att.com>
2024-11-16 14:32:37 -0500

    11.16.2024

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-d
 
 import './App.css';
 import Header from './components/Header';
-import HomePage from './pages/HomePage';
 import AboutPage from './pages/AboutPage';
 import EDFOraclePage from './pages/EDFOraclePage';
 import SQLPage from './pages/SQLPage';
@@ -16,9 +15,9 @@ import LandingPage from './pages/LandingPage'; // Import the LandingPage compone
 
 function App() {
   return (
-    <Router><div><Header /><Routes><Route exact path="/" element={<HomePage />} /><Route path="/about" element={<AboutPage />} /><Route path="/data" element={<EDFOraclePage />} /><Route path="/sql" element={<SQLPage />} /><Route path="/edf-ms-oracle" element={<EDFMSQueryPage />} /><Route path="/dashboards" element={<Delta_Dashboard />} /><Route path="/donuts" element={<Donuts />} /><Route path="/landing" element={<LandingPage />} /> {/* Add the route for LandingPage */}
+    <Router><div><Header /><Routes><Route path="/about" element={<AboutPage />} /><Route path="/data" element={<EDFOraclePage />} /><Route path="/sql" element={<SQLPage />} /><Route path="/edf-ms-oracle" element={<EDFMSQueryPage />} /><Route path="/dashboards" element={<Delta_Dashboard />} /><Route path="/donuts" element={<Donuts />} /><Route path="/landing" element={<LandingPage />} /> {/* Add the route for LandingPage */}
           <Route path="/src/pages/Donuts.css" element={<Navigate to="/landing" replace />} /> {/* Add a redirect route */}
-          <Route path="*" element={<Navigate to="/" replace />} /> {/* Add a default route */}
+          <Route path="*" element={<Navigate to="/landing" replace />} /> {/* Set default route to LandingPage */}
         </Routes></div></Router>
   );
 }
