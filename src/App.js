02ec7ab179937856dc915f3cdc11bd56925de5ba
Bobkoskie, Robert (rb868x) <rb868x@att.com>
2024-11-16 10:37:59 -0500

    11.16.2024

--- src/App.js
+++ src/App.js
@@ -12,11 +12,13 @@ import SQLPage from './pages/SQLPage';
 import EDFMSQueryPage from './pages/EDFMSQueryPage';
 import Delta_Dashboard from './pages/Delta_Dashboard';
 import Donuts from './pages/Donuts'; // Import the Donuts component
+import LandingPage from './pages/LandingPage'; // Import the LandingPage component
 
 function App() {
   return (
-    <Router><div><Header /><Routes><Route exact path="/" element={<HomePage />} /><Route path="/about" element={<AboutPage />} /><Route path="/data" element={<EDFOraclePage />} /><Route path="/sql" element={<SQLPage />} /><Route path="/edf-ms-oracle" element={<EDFMSQueryPage />} /><Route path="/dashboards" element={<Delta_Dashboard />} /><Route path="/src/pages/Donuts.css" element={<Donuts />} /> {/* Add the new route */}
-          <Route path="*" element={<Navigate to="/src/pages/Donuts.css" replace />} /> {/* Add a default route for testing */}
+    <Router><div><Header /><Routes><Route exact path="/" element={<LandingPage />} /> {/* Set LandingPage as the default landing page */}
+          <Route path="/home" element={<HomePage />} /><Route path="/about" element={<AboutPage />} /><Route path="/data" element={<EDFOraclePage />} /><Route path="/sql" element={<SQLPage />} /><Route path="/edf-ms-oracle" element={<EDFMSQueryPage />} /><Route path="/dashboards" element={<Delta_Dashboard />} /><Route path="/donuts" element={<Donuts />} /> {/* Corrected the path for Donuts component */}
+          <Route path="*" element={<Navigate to="/" replace />} /> {/* Redirect unknown paths to LandingPage */}
         </Routes></div></Router>
   );
 }
