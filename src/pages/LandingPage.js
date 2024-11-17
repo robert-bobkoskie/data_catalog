aeb35905b72c6cf4ba6be0fe374f08b40715a711
Bobkoskie, Robert (rb868x) <rb868x@att.com>
2024-11-16 20:50:10 -0500

    11.16.2024

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -9,6 +9,8 @@ const LandingPage = () => {
         This website is a partial view of the frontend as part of a full-stack Python Flask backend that brokers REST API HTTP requests to Mongo and Oracle DB databases, processes data to obtain metadata, and passes the results back to the frontend in HTTP responses. The data is presented to the user in clean tables in the front-end where the user can interact, drill down, and download data that provides insight into their product.
       </p><p className="landing-text">
         Most of the data is under Non-Disclosure Agreement (NDA). I have anonymized data to demonstrate some of the visualizations in the Dashboards page. The About page provides a high-level flow of the data pipeline. There is also a unique page that was my enticement of "virtual donuts" to a product team during one of my demos.
+      </p><p className="landing-text">
+        A user can interact with elements on the page by clicking, and by right-clicking and dragging to draw a selection box over a plot to display aggregated counts over that domain.
       </p></div>
   );
 };
